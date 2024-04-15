19b7bd68f42d033d5778bc4b567457e6d9aaf918
nikhil_shuklaa <nikhilshukla202005@gmail.com>
2024-04-15 05:51:18 +0530

    files uploaded

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Navbar from "@/components/navbar";
+import { Toaster } from "@/components/ui/toaster";
+import GA from "@/components/GA";
+import { ThemeProvider } from "@/components/theme-provider";
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+    title: "Convertify - Free Unlimited File Converter",
+    description: `Unleash your creativity with Convertify – the ultimate online tool for
+  unlimited and free multimedia conversion. Transform images, audio, and
+  videos effortlessly, without restrictions. Start converting now and
+  elevate your content like never before!`,
+    creator: "Nikhil Shukla",
+    keywords: "image converter, video converter, audio converter, unlimited image converter, unlimited video converter",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <html lang="en">
+            <GA GA_MEASUREMENT_ID="G-DNZK38BKBJ" />
+            <body className={inter.className}>
+                <ThemeProvider attribute="class" defaultTheme="system" enableSystem themes={["light", "dark"]}>
+                    <Navbar />
+                    <Toaster />
+                    <div className="pt-32 min-h-screen lg:pt-36 2xl:pt-44 container max-w-4xl lg:max-w-6xl 2xl:max-w-7xl">
+                        {children}
+                    </div>
+                </ThemeProvider>
+            </body>
+        </html>
+    );
+}
---
+++ app/page.tsx
@@ -0,0 +1,21 @@
+// imports
+import Dropzone from '@/components/dropzone';
+
+export default function Home() {
+  return (
+      <div className="space-y-16 pb-8">
+          {/* Title + Desc */}
+          <div className="space-y-6">
+              <h1 className="text-3xl md:text-5xl font-medium text-center">Free Unlimited File Converter</h1>
+              <p className="text-muted-foreground text-md md:text-lg text-center md:px-24 xl:px-44 2xl:px-52">
+                  Unleash your creativity with Convertify – the ultimate online tool for unlimited and free multimedia
+                  conversion. Transform images, audio, and videos effortlessly, without restrictions. Start converting
+                  now and elevate your content like never before!
+              </p>
+          </div>
+
+          {/* Upload Box */}
+          <Dropzone />
+      </div>
+  );
+}
